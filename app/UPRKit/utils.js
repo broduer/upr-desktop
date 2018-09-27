8f842ab0cfb59e083f9a3dc0bfdd5f0ab273b90e
Brendan Boyle <brendancboyle@me.com>
2018-09-27 13:38:49 -0400

    Added signature validation

--- app/UPRKit/utils.js
+++ app/UPRKit/utils.js
@@ -1,5 +1,8 @@
 import SendKeys from 'send-keys-native';
 import io from 'socket.io-client';
+import _ from 'lodash';
+import crypto from 'crypto';
+import pub from './certificates/public.key';
 
 export const SLIDE_UP = 'SlideUp';
 export const SLIDE_DOWN = 'SlideDown';
@@ -9,13 +12,22 @@ let socket;
 
 type messageType = {
   action: string,
-  holdfor: string
+  holdfor: string,
+  signature: string,
+  timestamp: string
 };
 
 function listenForEvents(token: string, holdFor: string) {
   socket = io('https://universalpresenterremote.com');
   socket.on(token, (message: messageType) => {
-    if (message.holdfor === holdFor) {
+    const validate: messageType = _.cloneDeep(message);
+    delete validate.signature;
+    const jsonMsg = JSON.stringify(validate);
+    const verify = crypto.createVerify('SHA256');
+    verify.write(jsonMsg);
+    verify.end();
+    const isValid = verify.verify(pub.key, message.signature, 'hex');
+    if (isValid && message.holdfor === holdFor) {
       switch (message.action) {
         case SLIDE_UP: {
           SendKeys.rightArrow();
