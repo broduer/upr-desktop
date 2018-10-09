955d20686d2b95f9d7a7da433fc3b96fdfe762f7
Brendan Boyle <brendancboyle@me.com>
2018-10-09 14:15:22 -0400

    Added Sentry

--- app/index.js
+++ app/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import * as Sentry from '@sentry/electron';
 import Root from './containers/Root';
 import { configureStore, history } from './store/configureStore';
 import './app.global.css';
+
+Sentry.init({
+  dsn: 'https://cf07bcc0594241ce8c7db854e0f3f65a@sentry.io/1297683'
+});
 
 const store = configureStore();
 
